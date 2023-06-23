385cf80917dfe6c54c623e5a9ea618aa50ff8b2f
Klara Schovankova <schovkla@fit.cvut.cz>
2023-06-23 14:09:32 +0200

    Create cookbook models

--- cookbook_app/models.py
+++ cookbook_app/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.utils import timezone
+from colorfield.fields import ColorField
 
-# Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=127);
+    note = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(editable=False)
+    modified = models.DateTimeField()
+    tags = models.ManyToManyField('Tag', related_name='recipes', blank=True)
+    ingredients = models.ManyToManyField('Ingredient', through='Amount', related_name='recipes', blank=True)
+
+    def save(self, *args, **kwargs):
+        """ On save, update timestamps"""
+        if not self.id:
+            self.created = timezone.now()
+        self.modified = timezone.now()
+        return super().save(*args, *kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=127)
+    color = ColorField(defaul='#0000FF')
+
+    def __str__(self):
+        return self.name
+
+
+class Step(models.Model):
+    description: models.TextField()
+    order: int
+
+    def __str__(self):
+        return self.description
+
+
+class Ingredient(models.Model):
+    name: models.CharField(max_length=127)
+
+    def __str__(self):
+        return self.name
+
+
+class Amount(models.Model):
+    value: float
+    recipe: models.ForeignKey(Recipe, on_delete=models.SET_NULL)
+    ingredient: models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    unit: models.CharField(max_length=63)
