e1454eacba03fc15c88813be13cbca75d854874d
Klara Schovankova <schovkla@fit.cvut.cz>
2023-06-25 00:08:10 +0200

    Create recipe page draft

--- cookbook_app/apps.py
+++ cookbook_app/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class CookbookAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cookbook_app'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "cookbook_app"
--- cookbook_app/views.py
+++ cookbook_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import DetailView
+
+from cookbook_app.models import Recipe
+
+
+class RecipeView(DetailView):
+    template_name = "cookbook_app/recipe.html"
+    model = Recipe
