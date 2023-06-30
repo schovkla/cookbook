6e9b588bdb00e9e66c2d48124fcfae3ef102eb0a
Klara Schovankova <schovkla@fit.cvut.cz>
2023-06-30 23:20:30 +0200

    Polish recipe detail page.

--- cookbook_app/views.py
+++ cookbook_app/views.py
@@ -8,10 +8,4 @@ from cookbook_app.models import Recipe
 class RecipeView(DetailView):
     template_name = "cookbook_app/recipe.html"
     model = Recipe
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        recipe = self.get_object()
-        context['steps'] = recipe.steps.all()
-        return context
-
+    context_object_name = "recipe"
