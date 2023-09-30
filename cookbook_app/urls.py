a08421617078d26f4b482b4400df7c78bd56b7e0
Klara Schovankova <schovkla@fit.cvut.cz>
2023-09-30 20:09:26 +0200

    Redirect home page to recipes.

--- cookbook_app/urls.py
+++ cookbook_app/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.views.generic import RedirectView
 
 from cookbook_app.views import RecipeView, RecipeListView, SearchRecipeView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url=reverse_lazy('recipes'), permanent=True)),
     path("recipe/<int:pk>/", RecipeView.as_view(), name="recipe"),
     path("recipes/", RecipeListView.as_view(), name="recipes"),
     path("search_recipes/", SearchRecipeView.as_view(), name='search_recipes'),
